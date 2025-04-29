85d661800ad68f51812e21a65e45f450bfdb267b
2vnohsk <cabesaje@gmail.com>
2025-04-28 22:29:14 -0500

    Update Bodega.java

--- Bodega.java
+++ Bodega.java
@@ -2,99 +2,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bodega {
-   
     private int id;
     private String direccion;
     private double capacidadMaximaToneladas;
     private int capacidadMaximaTrenes;
     private double usoActualToneladas;
     private int usoActualTrenes;
-    private String tipoCarga;
+    private String tipoCarga; 
     private List<Tren> trenes;
 
-    public Bodega() {
-        trenes = new ArrayList<>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public Bodega(int id, String direccion, double capacidadMaximaToneladas, int capacidadMaximaTrenes, String tipoCarga) {
         this.id = id;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
         this.direccion = direccion;
-    }
-
-    public double getCapacidadMaximaToneladas() {
-        return capacidadMaximaToneladas;
-    }
-
-    public void setCapacidadMaximaToneladas(double capacidadMaximaToneladas) {
         this.capacidadMaximaToneladas = capacidadMaximaToneladas;
-    }
-
-    public int getCapacidadMaximaTrenes() {
-        return capacidadMaximaTrenes;
-    }
-
-    public void setCapacidadMaximaTrenes(int capacidadMaximaTrenes) {
         this.capacidadMaximaTrenes = capacidadMaximaTrenes;
-    }
-
-    public double getUsoActualToneladas() {
-        return usoActualToneladas;
-    }
-
-    public int getUsoActualTrenes() {
-        return usoActualTrenes;
-    }
-
-    public String getTipoCarga() {
-        return tipoCarga;
-    }
-
-    public void setTipoCarga(String tipoCarga) {
         this.tipoCarga = tipoCarga;
+        this.trenes = new ArrayList<>();
+        this.usoActualToneladas = 0;
+        this.usoActualTrenes = 0;
     }
 
     public void agregarTren(Tren tren) {
-        if (usoActualTrenes < capacidadMaximaTrenes && 
-            usoActualToneladas + tren.getCargaActualToneladas() <= capacidadMaximaToneladas && 
+        if (usoActualTrenes < capacidadMaximaTrenes &&
+            usoActualToneladas + tren.getCapacidad() <= capacidadMaximaToneladas &&
             tren.getTipoCarga().equals(tipoCarga)) {
             
             trenes.add(tren);
-            usoActualToneladas += tren.getCargaActualToneladas();
+            usoActualToneladas += tren.getCapacidad();
             usoActualTrenes++;
-            System.out.println("Tren agregado a la bodega.");
+            System.out.println("Tren " + tren.getId() + " agregado a la bodega " + id + ".");
         } else {
-            System.out.println("No se puede agregar el tren a la bodega.");
+            System.out.println("No se puede agregar el tren " + tren.getId() + " a la bodega " + id + ".");
         }
     }
 
     public void darSalidaTren(Tren tren) {
         if (trenes.remove(tren)) {
-            usoActualToneladas -= tren.getCargaActualToneladas();
+            usoActualToneladas -= tren.getCapacidad();
             usoActualTrenes--;
-            System.out.println("Tren dado de salida de la bodega.");
+            tren.darSalida();
         } else {
-            System.out.println("El tren no se encuentra en la bodega.");
+            System.out.println("El tren " + tren.getId() + " no se encuentra en la bodega " + id + ".");
         }
     }
 
     public void mostrarInformacion() {
         System.out.println("ID de la bodega: " + id);
         System.out.println("Dirección: " + direccion);
-        System.out.println("Capacidad máxima (toneladas): " + capacidadMaximaToneladas);
-        System.out.println("Capacidad máxima (trenes): " + capacidadMaximaTrenes);
-        System.out.println("Uso actual (toneladas): " + usoActualToneladas);
-        System.out.println("Uso actual (trenes): " + usoActualTrenes);
+        System.out.println("Capacidad máxima toneladas: " + capacidadMaximaToneladas);
+        System.out.println("Capacidad máxima trenes: " + capacidadMaximaTrenes);
+        System.out.println("Uso actual toneladas: " + usoActualToneladas);
+        System.out.println("Uso actual trenes: " + usoActualTrenes);
         System.out.println("Tipo de carga: " + tipoCarga);
     }
-}
